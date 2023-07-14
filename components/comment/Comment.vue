7784a04815d0639a3d243fae7a2c3eb01cb2bc63
junnikym <junnikym@gmail.com>
2023-07-14 17:32:46 +0900

    add: comment in post page

---
+++ components/comment/Comment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="comment">
+    <script src="https://utteranc.es/client.js"
+        repo="junnikym/junnikym.github.io"
+        issue-term="pathname"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+	  </script>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Comment",
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
