07b867afe73f5e5773773d9dcf4b470fe1938e95
junnikym <junnikym@users.noreply.github.com>
2023-11-01 06:49:52 +0000

    deploy: c561cf63081807bc5b9de055b7ed392d912ebc0b

---
+++ _nuxt/static/1698821390/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjunnikym.github.io\u002F_nuxt\u002Fstatic\u002F1698821390",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fjunnikym.github.io\u002F",assetsPath:"\u002Fjunnikym.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
